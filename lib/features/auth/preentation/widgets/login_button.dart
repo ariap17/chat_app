ee58d3c97a6b9f08d8ed364d624d4723b21242d2
ariap17 <ayaelgendy126@gmail.com>
2024-12-02 00:46:19 +0200

    login

---
+++ lib/features/auth/preentation/widgets/login_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginButton extends StatelessWidget {
+  final String title;
+  final void Function() onTap;
+  const LoginButton({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.h,
+      width: 200.w ,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.r),
+        color: const Color.fromRGBO(0, 82, 218, 1.0,),
+      ),
+      child: TextButton(onPressed: onTap,
+          child: Text(title, style: const TextStyle(fontSize: 22,
+              color: Colors.white,
+            height: 1.5
+          ),),
+      ),
+    );
+  }
+}
